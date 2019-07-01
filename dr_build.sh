3092c8e65c0c785ae52095e409054c0d77a52318
mahesh.thundathil <mahesh.thundathil@digitalreasoning.com>
2019-07-01 08:45:42 -0500

    updating build

--- dr_build.sh
+++ dr_build.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
 set -e
+log_msg () {
+    echo
+    echo "[`date`] - BUILD SCRIPT - $1"
+}
+
+MINICONDA_HOME=$BUILD_DIR/miniconda3
+log_msg "Initializing Miniconda3 environment: $MINICONDA_HOME"
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh -b -p $MINICONDA_HOME
+export PATH=$MINICONDA_HOME/bin/:$PATH
+
+scl enable devtoolset-7 bash
+cd builder/conda
+export PYTORCH_FINAL_PACKAGE_DIR=$BUILD_DIR/artifacts
+export DESIRED_PYTHON=3.7
+export TORCH_CONDA_BUILD_FOLDER=pytorch-1.0.1
+export PYTORCH_REPO=pytorch
+export PYTORCH_BRANCH=v1.0.1
+./build_pytorch.sh 100 1.0.1 1 # cuda 10.0 pytorch 1.0.1 build_number 1
+
